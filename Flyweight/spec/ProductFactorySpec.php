5e913d7402673296ef6e6453bb87dc107ff1d129
jamescowie <james@jcowie.co.uk>
2014-08-11 20:23:03 +0100

    NotBeLike

--- Flyweight/spec/ProductFactorySpec.php
+++ Flyweight/spec/ProductFactorySpec.php
@@ -11,13 +11,13 @@ class ProductFactorySpec extends ObjectBehavior
     public function it_should_return_product_object_when_a_common_key_is_used()
     {
         $base = $this->getProducts('Product Name');
-        $this->getProducts('Product Name')->shouldBe($base);
+        $this->getProducts('Product Name')->shouldBeLike($base);
     }
 
     function it_should_return_a_new_obect_when_new_key_is_used()
     {
         $base = $this->getProducts('First Name');
-        $this->getProducts('Second Name')->shouldNotBe($base);
+        $this->getProducts('Second Name')->shouldNotBeLike($base);
     }
 
 }
